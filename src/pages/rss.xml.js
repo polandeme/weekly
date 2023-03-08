620a3f5569531e03885172080be9e896362139ec
polandeme <polandeme@gmail.com>
2023-03-08 16:36:43 +0800

    remove posts and publish EP001

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,9 +14,9 @@ posts.splice(15);
 
 export const get = () =>
   rss({
-    title: '潮流周刊',
-    description: '记录 Tw93 潮流前端的日常生活',
-    site: 'https://weekly.tw93.fun/',
+    title: '后厂村周刊',
+    description: '',
+    site: 'https://hcc1010.com/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
